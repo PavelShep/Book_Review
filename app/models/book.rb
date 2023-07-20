84897360eca37116d0b0749992768d02634bca89
Pavel Shapialevich <Pavel.Shapialevich@student.ajp.edu.pl>
2023-07-20 16:27:22 +0200

    add average rating for each book

--- app/models/book.rb
+++ app/models/book.rb
@@ -5,4 +5,14 @@ class Book < ApplicationRecord
   has_many :reviews
 
   has_one_attached :image
+
+  #need for average rating
+  def average_rating
+    if self.reviews.size > 0
+        self.reviews.average(:rating)
+    else
+        "There are no reviews"
+    end
+  end
+
 end
